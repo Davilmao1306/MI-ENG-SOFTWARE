5d091748c0db102617b0a74bf5bc408356b2941c
JefersonAlmeid4 <jefersonalmeidauefs@gmail.com>
2025-11-12 07:21:12 -0300

    add plano terapêutico

--- sistema/planos/urls.py
+++ sistema/planos/urls.py
@@ -9,16 +9,3 @@ router.register(r'planos-terapeuticos', PlanoTerapeuticoViewSet, basename='plano
 urlpatterns = [
     path('', include(router.urls)),
 ]
-
-# --- ISSO GERA AUTOMATICAMENTE AS SEGUINTES ROTAS ---
-# POST   /planos-terapeuticos/             (Criar Plano - UC19, RF008)
-# GET    /planos-terapeuticos/             (Listar Planos)
-# GET    /planos-terapeuticos/{id}/        (Ver Plano - RF008, RF011)
-# PUT    /planos-terapeuticos/{id}/        (Editar Plano - UC20, RF012)
-# PATCH  /planos-terapeuticos/{id}/        (Editar Plano - UC20, RF012)
-# DELETE /planos-terapeuticos/{id}/        (Remover Plano)
-#
-# --- E AS ROTAS CUSTOMIZADAS ---
-# POST   /planos-terapeuticos/{id}/anexar_arquivo/ (RF010, RF014)
-# GET    /planos-terapeuticos/{id}/historico/      (RF013)
-# POST   /planos-terapeuticos/{id}/assinar_terapeuta/
