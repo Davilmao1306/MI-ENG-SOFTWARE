d59b48a267d4c5f7d0232ebaf05c0c611b43b4d0
Davilmao1306 <ambengenharia964@gmail.com>
2025-12-05 10:29:04 -0300

    Add superuser creation view and update URLs temporario
    Added a temporary view to create a superuser and updated URL patterns.

--- sistema/sistema/urls.py
+++ sistema/sistema/urls.py
@@ -1,18 +1,41 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
-
-def home(request):
-    return HttpResponse("OK - Backend de Login e Cadastro ativo.")
-
+# --- FUNÇÃO TEMPORÁRIA PARA CRIAR ADMIN ---
+def criar_superusuario_view(request):
+    User = get_user_model()
+    email = "admin@neurolink.com"
+    senha = "AdminNeuro123!" # Senha forte
+    
+    if not User.objects.filter(email=email).exists():
+        # Cria o usuário se não existir
+        User.objects.create_superuser(
+            email=email,
+            password=senha,
+            nome="Administrador Geral" # Ajuste se seu model pedir outros campos
+        )
+        return HttpResponse(f"Sucesso! Admin criado.<br>Email: {email}<br>Senha: {senha}")
+    else:
+        return HttpResponse("O Admin já existe!")
+# ------------------------------------------
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('login/', include('contas.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('autenticacao/', include('autenticacao.urls')),
     path('cadastro/', include('cadastro.urls')),
-    path('vincular/', include('vinculos.urls')),
-    path("plano/", include("plano.urls")),
-    path("diario/", include("diario.urls")),
+    path('login/', include('contas.urls')), 
+    path('vinculos/', include('vinculos.urls')),
+    path('plano/', include('plano.urls')),
+    path('diario/', include('diario.urls')),
+    
+    # Rota secreta temporária
+    path('criar-admin-secreto/', criar_superusuario_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
